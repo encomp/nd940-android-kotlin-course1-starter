224ced86840cfb1437bfda2cbe89d96a141904ff
Edgar Rico <edgar.martinez.rico@gmail.com>
2021-09-26 20:25:12 -0700

    Fix main activity.

--- starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -1,22 +1,33 @@
 package com.udacity.shoestore
 
-import androidx.appcompat.app.AppCompatActivity
-import androidx.activity.compose.setContent
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import android.os.Bundle
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import com.udacity.shoestore.ui.AppTheme
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.udacity.shoestore.databinding.ActivityMainBinding
 
-import timber.log.Timber
-
+/** Defines the main activity of the Shoe app. */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Timber.plant(Timber.DebugTree())
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(
+            this, R.layout.activity_main
+        )
+        setSupportActionBar(binding.toolbar)
+        val navController = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupWithNavController(
+            binding.toolbar,
+            navController,
+            AppBarConfiguration(navGraph = navController.graph)
+        )
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp()
     }
 }
 
