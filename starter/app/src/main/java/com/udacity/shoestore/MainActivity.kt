c7426969bb6031bf4082b2334d98336b4e8185f7
encomp <edgar.martinez.rico@gmail.com>
2021-09-26 19:19:14 -0700

    Add main fragment.

--- starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ starter/app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.udacity.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import android.os.Bundle
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import com.udacity.shoestore.ui.AppTheme
+
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
@@ -12,3 +19,29 @@ class MainActivity : AppCompatActivity() {
         Timber.plant(Timber.DebugTree())
     }
 }
+
+//class MainActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContent {
+//            AppTheme {
+//                Surface(color = MaterialTheme.colors.background) {
+//                    Greeting("Android")
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//@Composable
+//fun Greeting(name: String) {
+//    Text(text = "Hello $name!")
+//}
+
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    BasicsCodelabTheme {
+//        Greeting("Android")
+//    }
+//}
