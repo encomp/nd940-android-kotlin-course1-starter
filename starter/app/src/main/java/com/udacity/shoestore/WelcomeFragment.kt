c3ee519f54a915c815bf20cf06ab4d55a940490c
Edgar Rico <edgar.martinez.rico@gmail.com>
2021-09-27 20:16:25 -0700

    Add compose to the welcome fragment.

--- starter/app/src/main/java/com/udacity/shoestore/WelcomeFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/WelcomeFragment.kt
@@ -1,59 +1,65 @@
 package com.udacity.shoestore
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.google.android.material.composethemeadapter.MdcTheme
+import com.udacity.shoestore.databinding.FragmentWelcomeBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [WelcomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+/** Defines the welcome fragment. */
 class WelcomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentWelcomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment WelcomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            WelcomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_welcome,
+            container,
+            false
+        )
+        val composeView = binding.root.findViewById<ComposeView>(R.id.composeView)
+        composeView.setContent {
+            MdcTheme() {
+                Greeting()
             }
+        }
+        return binding.root
+    }
+}
+
+@Composable
+private fun Greeting() {
+    Row(horizontalArrangement = Arrangement.Center) {
+        Column() {
+            Text(
+                text = stringResource(R.string.welcomeTextView),
+                modifier = Modifier.padding(24.dp)
+            )
+            Text(
+                text = stringResource(R.string.app_name),
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colors.secondaryVariant,
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold,
+            )
+        }
     }
 }
