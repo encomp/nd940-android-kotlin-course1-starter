99539b19eda01c4c9be747c2d26b5158349a26e2
Edgar Rico <edgar.martinez.rico@gmail.com>
2021-09-29 18:52:38 -0700

    Add the list of shoes fragment.

---
+++ starter/app/src/main/java/com/udacity/shoestore/ShoeListFragment.kt
@@ -0,0 +1,29 @@
+package com.udacity.shoestore
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.databinding.FragmentInstructionsBinding
+import com.udacity.shoestore.databinding.FragmentShoeListBinding
+
+/** List of shoes fragment. */
+class ShoeListFragment : Fragment() {
+
+    private lateinit var binding: FragmentShoeListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_shoe_list,
+            container,
+            false
+        )
+        return binding.root
+    }
+}
